3111d2d85ea395f401a541b9e149a457cf42d3b1
Zachary Alexander <86435089+zachsplat@users.noreply.github.com>
2025-10-19 20:00:00 -0500

    wire up WAL (using a gross global, fix later)

--- src/db.cpp
+++ src/db.cpp
@@ -1,4 +1,5 @@
 #include "lsm/db.h"
+#include "lsm/wal.h"
 #include <filesystem>
 #include <algorithm>
 
@@ -6,43 +7,53 @@ namespace fs = std::filesystem;
 
 namespace lsm {
 
+// gross but I don't want to restructure the header right now
+static std::unique_ptr<WAL> g_wal;
+
 DB::DB(const Options& opts)
     : opts_(opts), memtable_(opts.memtable_size), next_sst_id_(0) {
     fs::create_directories(opts.dir);
     load_existing();
+    g_wal = std::make_unique<WAL>(opts.dir + "/wal.log");
+    // replay WAL into memtable
+    g_wal->replay([this](bool is_del, const std::string& key, const std::string& val) {
+        if (is_del) memtable_.remove(key);
+        else memtable_.put(key, val);
+    });
 }
 
 DB::~DB() {
-    // flush remaining memtable
     if (memtable_.count() > 0) {
         flush();
     }
+    g_wal.reset();
 }
 
 void DB::put(const std::string& key, const std::string& value) {
     std::lock_guard<std::mutex> lock(mu_);
+    if (g_wal) g_wal->log_put(key, value);
     memtable_.put(key, value);
     maybe_flush();
 }
 
 std::optional<std::string> DB::get(const std::string& key) {
     std::lock_guard<std::mutex> lock(mu_);
-
-    // check memtable first
     auto val = memtable_.get(key);
     if (val) return val;
 
-    // check sstables newest to oldest
     for (auto it = sstables_.rbegin(); it != sstables_.rend(); ++it) {
         auto v = (*it)->get(key);
-        if (v) return v;
+        if (v) {
+            if (Memtable::is_tombstone(*v)) return std::nullopt;
+            return v;
+        }
     }
-
     return std::nullopt;
 }
 
 void DB::remove(const std::string& key) {
     std::lock_guard<std::mutex> lock(mu_);
+    if (g_wal) g_wal->log_delete(key);
     memtable_.remove(key);
     maybe_flush();
 }
@@ -52,22 +63,17 @@ void DB::flush() {
 
     std::string path = sst_path(next_sst_id_++);
     SSTableWriter writer(path);
-
     for (auto it = memtable_.begin(); it != memtable_.end(); ++it) {
         writer.add(it->first, it->second);
     }
     writer.finish();
-
     sstables_.push_back(std::make_unique<SSTableReader>(path));
     memtable_.clear();
-
-    // maybe_compact();  // not done yet
+    if (g_wal) g_wal->reset();
 }
 
 void DB::maybe_flush() {
-    if (memtable_.is_full()) {
-        flush();
-    }
+    if (memtable_.is_full()) flush();
 }
 
 std::string DB::sst_path(int id) {
@@ -90,8 +96,7 @@ void DB::load_existing() {
 }
 
 void DB::maybe_compact() {
-    // TODO: size-tiered compaction
-    // merge overlapping sstables, remove tombstones
+    // TODO
 }
 
 }
